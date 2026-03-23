7e7ca68ffb2c37b331b931b9a939e641e0122b65
D-4costa <danielleelisabeth01@gmail.com>
2026-03-23 14:48:28 -0500

    Create products.ts

---
+++ app/data/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chocolate Cake",
+    price: 12,
+    category: "cake",
+    image: "/chocolate-cake.jpg",
+  },
+  {
+    id: 2,
+    name: "Cupcakes",
+    price: 8,
+    category: "cupcake",
+    image: "/cupcakes.jpg",
+  },
+  {
+    id: 3,
+    name: "Macarons",
+    price: 16,
+    category: "premium",
+    image: "/macarons.jpg",
+  },
+  {
+    id: 4,
+    name: "Strawberry Cake",
+    price: 14,
+    category: "cake",
+    image: "/strawberry-cake.jpg",
+  },
+];
